8e581d892893339b5d36102e8d6e85c4486b3172
wudashan <wudashan0825@foxmail.com>
2019-03-17 23:53:07 +0800

    优化用例

--- src/main/java/profiler/ProfilerTest.java
+++ src/main/java/profiler/ProfilerTest.java
@@ -15,8 +15,8 @@ public class ProfilerTest {
 
         try {
             callRpcInterface();
-            sendMessage();
             saveData2Db();
+            sendMessage();
         } finally {
             Profiler.exit();
             System.out.println(Profiler.dump());
@@ -28,17 +28,25 @@ public class ProfilerTest {
     private static void saveData2Db() throws InterruptedException {
         Profiler.enter("模拟保存数据到数据库");
         Thread.sleep(RandomUtils.nextInt(100));
+        doSomeThing();
         Profiler.exit();
     }
 
     private static void sendMessage() throws InterruptedException {
         Profiler.enter("模拟发送消息");
         Thread.sleep(RandomUtils.nextInt(100));
+        doSomeThing();
         Profiler.exit();
     }
 
     private static void callRpcInterface() throws InterruptedException {
         Profiler.enter("模拟调用RPC接口");
+        Thread.sleep(RandomUtils.nextInt(100));
+        Profiler.exit();
+    }
+
+    private static void doSomeThing() throws InterruptedException {
+        Profiler.enter("模拟操作");
         Thread.sleep(RandomUtils.nextInt(100));
         Profiler.exit();
     }
